f24612b72278b5885987dc64772164e17f270f15
GaboRex <gaboneme11@gmail.com>
2022-09-13 17:04:01 -0400

    fix api

--- src/config/api.js
+++ src/config/api.js
@@ -1,5 +1,9 @@
 import axios from 'axios'
-const API_URL='http://localhost:5000/api/v1/cliente'
+const API_URL='https://betterme2022.herokuapp.com/api/v1/cliente'
 export default axios.create({
-    baseURL: API_URL
+    baseURL: API_URL,
+    headers: {
+        //'Content-Type': 'application/json'
+        ' Access-Control-Allow-Origin': '*'
+    }
 })
